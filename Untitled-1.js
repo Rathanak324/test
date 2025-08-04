e66e6085ba77337be0e6ef41bf3afaed817b9140
Rathanak324 <sereywathrathanak@gmail.com>
2025-08-04 10:09:13 +0700

    test code in java for week 1

---
+++ Untitled-1.js
@@ -0,0 +1,26 @@
+// Print a greeting message
+console.log("Hello, welcome to JavaScript programming!");
+
+// Declare and initialize two numbers
+let number1 = 10;
+let number2 = 5;
+
+// Perform basic arithmetic operations
+let sum = number1 + number2;
+let difference = number1 - number2;
+let product = number1 * number2;
+let quotient = number1 / number2;
+
+// Display the results
+console.log("Number 1: " + number1);
+console.log("Number 2: " + number2);
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
+
+console.log("Thank you for running this script!");
+// End of script
+
+let yourName = "User";
+console.log("Goodbye, " + yourName + "!");
